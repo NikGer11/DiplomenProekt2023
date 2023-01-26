e0988cb72c62122da98778deda11bfd781a8f554
alextakov <alekscvetanov5@gmail.com>
2023-01-26 14:13:18 +0200

    yes

---
+++ VinylWorld/VinylWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VinylWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
